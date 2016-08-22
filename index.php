df3fa46420670241a579076f3b1a675d2c5c5cc7
Abhijeet Mohan <void.aby@gmail.com>
2016-08-22 23:59:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 define('APP_PATH', __DIR__.'/app');
 define('APP_ROOT', __DIR__);
 
+// require all files here
 require APP_ROOT.'/vendor/autoload.php';
 require "app/config/db.php";
 require "app/config/app.php";
